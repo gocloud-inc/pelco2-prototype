9b02620b4280998c270878afab43e2e0e165d4ca
GitMarkPaul <markpaulcaneda1997@gmail.com>
2023-05-31 14:22:27 +0800

    ui updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,14 @@
 'use strict';
 
 import * as Turbo from "@hotwired/turbo";
-import { Modal } from "bootstrap";
+import { Modal, Dropdown } from "bootstrap";
 import Button from "./buttons";
-import Dropdown from "./dropdown";
 import Greeting from "./greeting";
 import Theme from "./theme";
 import Header from "./header";
 
 const turboLoad = () => {
     Button.apply(Button);
-    Dropdown.apply(Dropdown);
     Greeting.apply(Greeting);
     Theme.apply(Theme);
     Header.apply(Header);
